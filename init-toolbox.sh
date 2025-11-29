0a2c361b9010b874f7e693339a72cde26d3dc4a3
azwaw111 <wawaaziz2004@gmail.com>
2025-11-29 21:39:29 +0100

    Add files via upload

--- init-toolbox.sh
+++ init-toolbox.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+# Vérifier si le dossier existe
+if [ ! -d ".sh-toolbox" ]; then
+    mkdir ".sh-toolbox" || { echo "Erreur : impossible de créer le dossier"; exit 1; }
+    echo "Dossier .sh-toolbox créé"
+else
+    echo "Le dossier .sh-toolbox existe déjà"
+fi
+
+# Vérifier si le fichier existe
+if [ ! -f ".sh-toolbox/archives" ]; then
+    echo 0 > ".sh-toolbox/archives" || { echo "Erreur : impossible de créer le fichier"; exit 1; }
+    echo "Fichier archives créé"
+else
+    echo "Le fichier archives existe déjà"
+fi
+
+# Vérifier que le dossier contient uniquement archives
+contenu=$(ls -A .sh-toolbox)
+if [ "$contenu" != "archives" ]; then
+    echo "Erreur : le dossier .sh-toolbox contient autre chose que le fichier archives"
+    exit 2
+fi
+
+echo "Initialisation réussie"
+exit 0
