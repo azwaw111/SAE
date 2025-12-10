c68d1f841a45273fab2d1de90494919ffde17861
azwaw111 <wawaaziz2004@gmail.com>
2025-12-10 02:14:01 +0100

    Implement Vigenère encryption and decryption functions

--- C/crypto.c
+++ C/crypto.c
@@ -1,7 +1,41 @@
 #include "crypto.h"
 
-
 int indice_base64(char caractere) {
     char *p = strchr(ALPHABET, caractere);
     return p ? (int)(p - ALPHABET) : -1;
 }
+
+
+void vigenereEnc(const char *entree, const char *cle, char *sortie) {
+    unsigned int i, taille = strlen(cle);
+    for (i = 0; entree[i] != '\0'; i++) {
+        if (entree[i] == '=') {
+            sortie[i] = '=';
+            continue;
+        }
+        int indiceCaractereEntree = indice_base64(entree[i]);
+        int indiceCaractereCle = indice_base64(cle[i % taille]);
+        if (indiceCaractereEntree < 0 || indiceCaractereCle < 0) {
+            sortie[i] = entree[i];
+            continue;
+        }
+        int indiceCaractereChiffre = (indiceCaractereEntree + indiceCaractereCle) % 64;
+        sortie[i] = ALPHABET[indiceCaractereChiffre];
+    }
+    sortie[i] = '\0';
+}
+
+
+
+void vigenereDec(const char *entree, const char *cle, char *sortie) {
+    unsigned int i, taille = strlen(cle);
+    char cleInverse[taille + 1];
+    for (i = 0; cle[i] != '\0'; i++) {
+        int indiceCaractereCle = indice_base64(cle[i]);
+        int indiceCleInverse = (64 - indiceCaractereCle) % 64;
+        cleInverse[i] = ALPHABET[indiceCleInverse];
+    }
+    cleInverse[i] = '\0';
+    vigenereEnc(entree, cleInverse, sortie);
+}
+
